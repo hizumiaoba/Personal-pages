d3f6fe2a3e25e70164a257e90d62bbe057f50468
hizumiaoba-tablet <hizumiaoba@gmail.com>
2022-04-18 22:44:45 +0900

    chore: add func

--- docs/publish-novel-search-system/main.js
+++ docs/publish-novel-search-system/main.js
@@ -1,4 +1,8 @@
 function readAllPublished() {
     const json = require('./allPublished.json');
-    
+    let allPubsElement = json.name;
+    let allPubs = [];
+    for (let i = 0; i < allPubsElement.length; i++) {
+        allPubs.push(allPubsElement[i]);
+    }
 }
