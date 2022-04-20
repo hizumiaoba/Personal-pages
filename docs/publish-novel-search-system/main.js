3e1a12d36e946028191b6a9c4828f859a03c267a
hizumiaoba-tablet <hizumiaoba@gmail.com>
2022-04-20 17:24:15 +0900

    chore: method changed

--- docs/publish-novel-search-system/main.js
+++ docs/publish-novel-search-system/main.js
@@ -1,37 +1,30 @@
 
-document.onload = function() {
-    const allPubs = readAllPublished();
-    const tbodyElement = document.getElementById('tbody');
-    for (let i = 0; i < allPubs.length; i++) {
-        tbodyElement.appendChild(createTableBody(allPubs[i]));
-    }
+function onLoadFunc() {
+    fetch('./allPublished.json').then(response => response.json())
+        .then(json => {
+            const namesArray = json.titles;
+            const workDetails = json.detail;
+            
+        });
 }
 
-function readAllPublished() {
-    const json = require('./allPublished.json');
-    let allPubsElement = json.name;
-    let allPubs = [];
-    for (let i = 0; i < allPubsElement.length; i++) {
-        allPubs.push(allPubsElement[i]);
-    }
-    return allPubs;
-}
 
-function createTableBody(detail) {
+function createTableBody(detail, titles) {
     const trElement = document.createElement('tr');
     const trNumber = document.createElement('td');
-    const trName = document.createElement('td');
     const trTitle = document.createElement('td');
     const trUploadDate = document.createElement('td');
     const trIdols = document.createElement('td');
     const trSeries = document.createElement('td');
     const trPrimary = document.createElement('td');
     trNumber.style.textAlign = 'center';
-    trName.style.textAlign = 'center';
     trTitle.style.textAlign = 'center';
     trUploadDate.style.textAlign = 'center';
     trIdols.style.textAlign = 'center';
     trSeries.style.textAlign = 'center';
     trPrimary.style.textAlign = 'center';
-    trNumber.innerHTML = detail.number;
+    trNumber.innerText = detail.number;
+    trTitle.innerText = titles[detail.number - 1];
+    trUploadDate.innerText = detail.pubDate;
+    trIdols.innerText = detail.idols;
 }
