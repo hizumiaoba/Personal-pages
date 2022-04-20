9b3bc02f613f6e3bd2ac69a02c93816b6b074d0f
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-04-20 22:57:30 +0900

    complete logic

--- docs/publish-novel-search-system/main.js
+++ docs/publish-novel-search-system/main.js
@@ -4,27 +4,45 @@ function onLoadFunc() {
         .then(json => {
             const namesArray = json.titles;
             const workDetails = json.detail;
-            
+            for(let i = 0; i < namesArray.length; i++) {
+                const trElement = createTableBody(workDetails, i);
+                document.getElementById('detailTable').appendChild(trElement);
+            }
         });
 }
 
 
-function createTableBody(detail, titles) {
+function createTableBody(detail, num) {
+    console.log(num);
+    console.log(detail);
+    const index = num + 1;
+    console.log(num);
     const trElement = document.createElement('tr');
-    const trNumber = document.createElement('td');
-    const trTitle = document.createElement('td');
-    const trUploadDate = document.createElement('td');
-    const trIdols = document.createElement('td');
-    const trSeries = document.createElement('td');
-    const trPrimary = document.createElement('td');
-    trNumber.style.textAlign = 'center';
-    trTitle.style.textAlign = 'center';
-    trUploadDate.style.textAlign = 'center';
-    trIdols.style.textAlign = 'center';
-    trSeries.style.textAlign = 'center';
-    trPrimary.style.textAlign = 'center';
-    trNumber.innerText = detail.number;
-    trTitle.innerText = titles[detail.number - 1];
-    trUploadDate.innerText = detail.pubDate;
-    trIdols.innerText = detail.idols;
+    const tdNumber = document.createElement('td');
+    const tdTitle = document.createElement('td');
+    const tdUploadDate = document.createElement('td');
+    const tdIdols = document.createElement('td');
+    const tdSeries = document.createElement('td');
+    const tdPrimary = document.createElement('td');
+    tdNumber.style.textAlign = 'center';
+    tdTitle.style.textAlign = 'center';
+    tdUploadDate.style.textAlign = 'center';
+    tdIdols.style.textAlign = 'center';
+    tdSeries.style.textAlign = 'center';
+    tdPrimary.style.textAlign = 'center';
+    tdNumber.innerText = index;
+    tdTitle.innerText = detail[num].title;
+    tdUploadDate.innerText = detail[num].pubDate;
+    tdIdols.innerText = detail[num].idols;
+    tdSeries.innerText = detail[num].series;
+    tdPrimary.innerText = detail[num].primary;
+    trElement.appendChild(tdNumber);
+    trElement.appendChild(tdTitle);
+    trElement.appendChild(tdUploadDate);
+    trElement.appendChild(tdIdols);
+    trElement.appendChild(tdSeries);
+    trElement.appendChild(tdPrimary);
+    console.log(trElement);
+
+    return trElement;
 }
