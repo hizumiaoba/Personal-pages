aa31b913908e846665f3bc588db8f4c01c56031c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-05-21 21:49:59 +0900

    fix: add links each title

--- docs/publish-novel-search-system/main.js
+++ docs/publish-novel-search-system/main.js
@@ -4,19 +4,21 @@ function onLoadFunc() {
         .then(json => {
             const namesArray = json.titles;
             const workDetails = json.detail;
-            for(let i = 0; i <= namesArray.length; i++) {
-                const trElement = createTableBody(workDetails, i);
+            const links = json.links;
+            for(let i = 0; i < namesArray.length; i++) {
+                const trElement = createTableBody(workDetails, links, i);
                 document.getElementById('detailTable').appendChild(trElement);
             }
         });
 }
 
 
-function createTableBody(detail, num) {
+function createTableBody(detail, links, num) {
     console.log(num);
     console.log(detail);
     const index = num + 1;
-    console.log(num);
+    var link = document.createElement('a');
+    link.setAttribute('href', links[num]);
     const trElement = document.createElement('tr');
     const tdNumber = document.createElement('td');
     const tdTitle = document.createElement('td');
@@ -26,12 +28,14 @@ function createTableBody(detail, num) {
     const tdPrimary = document.createElement('td');
     tdNumber.style.textAlign = 'center';
     tdTitle.style.textAlign = 'center';
+    tdTitle.id = 'title-' + index;
     tdUploadDate.style.textAlign = 'center';
     tdIdols.style.textAlign = 'center';
     tdSeries.style.textAlign = 'center';
     tdPrimary.style.textAlign = 'center';
     tdNumber.innerText = index;
-    tdTitle.innerText = detail[num].title;
+    link.innerText = detail[num].title;
+    tdTitle.append(link);
     tdUploadDate.innerText = detail[num].pubDate;
     tdIdols.innerText = detail[num].idols.toString().replaceAll(",", ", ");
     tdSeries.innerText = detail[num].series;
